2f4233e111dbf877b675bbd0434caed3e643533c
Ramiro Cosa <ramirocosa@gmail.com>
2024-06-03 10:51:53 -0300

    llamadas a dashboard

--- src/components/puntuar/Selects.jsx
+++ src/components/puntuar/Selects.jsx
@@ -1,50 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../styles/puntuar.css";
-import { useState } from "react";
 import PuntuarTable from "./PuntuarTable";
 import SelectComponent from "./SelectComponent";
 
-const genres = [
-  { id: 1, name: "Animation" },
-  { id: 2, name: "Children's" },
-  { id: 3, name: "Comedy" },
-  { id: 4, name: "Action" },
-  { id: 5, name: "Adventure" },
-  { id: 6, name: "Thriller" },
-  { id: 7, name: "Drama" },
-  { id: 8, name: "Crime" },
-  { id: 9, name: "Sci-Fi" },
-  { id: 10, name: "War" },
-  { id: 11, name: "Romance" },
-  { id: 12, name: "Horror" },
-  { id: 13, name: "Musical" },
-  { id: 14, name: "Documentary" },
-  { id: 15, name: "Western" },
-  { id: 16, name: "Fantasy" },
-  { id: 17, name: "Film-Noir" },
-  { id: 18, name: "Mystery" },
-];
-
-const Selects = () => {
-  const [data, setData] = useState(null);
-  const [genero, setGenero] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  const handleGeneroChange = (event) => setGenero(event.target.value);
-
-  const fetchData = (url) => {
-    setLoading(true);
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setData(data))
-      .finally(() => setLoading(false));
-  };
-
-  const handleSearch = () => {
-    let url = `http://localhost:3000/api/peliculas/genre/${genero}`;
-    fetchData(url);
-  };
-
+const Selects = ({
+  data,
+  genero,
+  genres,
+  loading,
+  handleGeneroChange,
+  handleSearch,
+}) => {
   return (
     <div className="container mt-5">
       <SelectComponent
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
+import axios from "axios";
+
 import AnalisisComponent from "../components/AnalisisComponent";
 import PuntuarComponent from "../components/puntuar/PuntuarComponent";
 import Sidebar from "../components/sidebar/Sidebar";
@@ -8,15 +10,57 @@ import "../styles/dashboard.css";
 const Dashboard = () => {
   const [activeComponent, setActiveComponent] = useState("puntuar");
   const { user } = useAuth();
-  console.log(user);
+  const [data, setData] = useState(null);
+  const [genero, setGenero] = useState("");
+  const [genres, setGenres] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const fetchGenres = async () => {
+      try {
+        const { data: genresData } = await axios.get(
+          "http://localhost:3000/api/getgenres"
+        );
+        setGenres(genresData);
+      } catch (error) {
+        console.error("Error fetching genres:", error);
+      }
+    };
 
+    fetchGenres();
+  }, []);
+
+  const handleGeneroChange = (event) => setGenero(event.target.value);
+
+  const fetchData = async (url) => {
+    setLoading(true);
+    try {
+      const { data: fetchedData } = await axios.get(url);
+      setData(fetchedData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSearch = () => {
+    const url = `http://localhost:3000/api/peliculas/genre/${genero}`;
+    fetchData(url);
+  };
   return (
     <div className="dashboard-container">
       <Sidebar setActiveComponent={setActiveComponent} />
       <div className="main-container">
         {activeComponent === "puntuar" && <PuntuarComponent />}
         {activeComponent === "analisis" && <AnalisisComponent />}
-        <Selects></Selects>
+        <Selects
+          data={data}
+          genero={genero}
+          genres={genres}
+          loading={loading}
+          handleGeneroChange={handleGeneroChange}
+          handleSearch={handleSearch}
+        />
       </div>
     </div>
   );
