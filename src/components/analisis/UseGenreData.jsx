3c8662b51fd23d81e180693b5f739e31b34710c8
Ramiro Cosa <ramirocosa@gmail.com>
2024-06-05 23:40:31 -0300

    Refactoring en analisis

--- src/components/analisis/UseGenreData.jsx
+++ src/components/analisis/UseGenreData.jsx
@@ -1,52 +1,11 @@
-import { useEffect, useState } from "react";
-import { getGenres, getGenreAnalysis } from "../../services/api";
+import { useState } from "react";
+import useGenres from "../../helpers/useGenres";
+import useGenreAnalysis from "../../helpers/useGenreAnalysis";
 
 const useGenreData = () => {
-  const [data, setData] = useState([]);
-  const [genres, setGenres] = useState([]);
   const [selectedGenre, setSelectedGenre] = useState("");
-  const [data2, setData2] = useState({});
-
-  useEffect(() => {
-    const fetchGenres = async () => {
-      try {
-        const response = await getGenres();
-        const result = response.data;
-        setGenres(result);
-        if (result.length > 0) {
-          setSelectedGenre(result[0].name);
-        }
-      } catch (error) {
-        console.error("Error fetching genres", error);
-      }
-    };
-
-    fetchGenres();
-  }, []);
-
-  useEffect(() => {
-    if (!selectedGenre) return;
-
-    const fetchData = async () => {
-      try {
-        const response = await getGenreAnalysis(selectedGenre);
-        const result = response.data;
-
-        // Aquí accedes a averageRatingByYear si existe en result
-        const { averageRatingByYear, votosPorRating } = result;
-        setData(averageRatingByYear);
-        setData2(votosPorRating);
-        if (averageRatingByYear && Array.isArray(averageRatingByYear)) {
-          console.log(votosPorRating);
-          // Puedes hacer lo que necesites con el array averageRatingByYear
-        }
-      } catch (error) {
-        console.error("Error fetching the data", error);
-      }
-    };
-
-    fetchData();
-  }, [selectedGenre]);
+  const genres = useGenres();
+  const { data, data2 } = useGenreAnalysis(selectedGenre);
 
   return { data, data2, genres, selectedGenre, setSelectedGenre };
 };
